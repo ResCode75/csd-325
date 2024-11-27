e9da625684bbf3f8141aa800501f1f1e68b7de71
ResCode75 <rachel.emily002@gmail.com>
2024-11-26 22:45:37 -0500

    7.1 assignment files 11/26/24

--- module-7/city_functions.py
+++ module-7/city_functions.py
@@ -2,6 +2,7 @@
 
 def city_country(city, country, population= None, language= None ):
    
+   #changes returned string based on arguments sent
    if population == None and language == None:
       city_and_country = f"{city}, {country}."
    elif population == None:
@@ -12,14 +13,14 @@ def city_country(city, country, population= None, language= None ):
       city_and_country = f'{city}, {country} -population {population}, {language}'
    
 
-      
-   
    return city_and_country
 
 
-
+# city, state
 print(city_country( city = "Tokyo", country = "Japan"))
 
+#city, state, population
 print(city_country(city = "Berlin", country = "Germany", population = "3,576,870"))
 
+#city, state, population, language
 print(city_country(city = "Paris", country = "France", population = "2,138,551", language = "French"))

ea8cc45402dc67ad33626966dba39c1600d68860
ResCode75 <rachel.emily002@gmail.com>
2024-12-07 20:48:51 -0500

    9.2 Assignment files 12/7/2024

---
+++ module-9/shaw9.2program.py
@@ -0,0 +1,25 @@
+#Rachel Shaw - 9.2 Assignment - 12/7/2024
+
+import requests
+import json
+
+# GET request for API
+response = requests.get("https://www.dnd5eapi.co/api/ability-scores")
+
+# print connection status code
+print(f"--CONNECTION TEST--\n{response.status_code}\n")
+
+ # D&D 5th Edition ability scores as unformatted json 
+print(f"--ABILITY SCORES JSON--\n{response.json()}\n")
+
+# Function to convert response into a formatted JSON string
+def jprint(obj):
+    text = json.dumps(obj, sort_keys = True, indent = 4)
+    print(f'--FORMATTED JSON--\n{text}')
+
+# print formatted D&D 5th Edition ability scores as a formatted JSON string
+jprint(response.json())
+
+
+
+
